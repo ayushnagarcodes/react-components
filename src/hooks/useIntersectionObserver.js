7bc776bac2785728dfff5713dd81febe4e293914
Ayush Nagar <ayushnagar15tfe@gmail.com>
2024-06-06 20:43:10 +0530

    Fixed bug in useIntersectionObserver hook

--- src/hooks/useIntersectionObserver.js
+++ src/hooks/useIntersectionObserver.js
@@ -3,21 +3,24 @@ import { useCallback, useRef } from "react";
 function useIntersectionObserver(handleFetch) {
     const observer = useRef(null);
 
-    const intersectionRef = useCallback((node) => {
-        if (!node) return;
-        if (observer.current) observer.current.disconnect();
+    const intersectionRef = useCallback(
+        (node) => {
+            if (!node) return;
+            if (observer.current) observer.current.disconnect();
 
-        observer.current = new IntersectionObserver(
-            (entries) => {
-                if (entries[0].isIntersecting) handleFetch();
-            },
-            {
-                threshold: 0.9,
-            }
-        );
+            observer.current = new IntersectionObserver(
+                (entries) => {
+                    if (entries[0].isIntersecting) handleFetch();
+                },
+                {
+                    threshold: 0.9,
+                }
+            );
 
-        observer.current.observe(node);
-    });
+            observer.current.observe(node);
+        },
+        [handleFetch]
+    );
 
     return intersectionRef;
 }
